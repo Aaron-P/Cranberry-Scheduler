cf4c511e3129ad52aa866170cbd3684c469e7947
Aaron <apapp@siue.edu>
2012-02-14 22:19:36 -0600

    Session handler changed to singlton

--- classes/SessionHandler.class.php
+++ classes/SessionHandler.class.php
@@ -1,7 +1,33 @@
 <?php
-
-
-
-
-
+class SessionSingleton
+{
+	private static $sessionInstance;
+	private function __construct()
+	{
+		session_start();
+	}
+	protected static function Instance()
+	{
+		if (!self::$sessionInstance)
+			self::$sessionInstance = new SessionSingleton();
+		return self::$sessionInstance;
+	} 
+	public function get($var)
+	{
+		if (isset($_SESSION[$var]))
+			return $_SESSION[$var];
+		return null;
+	}
+	public function set($var, $val)
+	{
+		$_SESSION[$var] = $val;
+	}
+}
+class SessionHandler extends SessionSingleton
+{
+	public function __construct()
+	{
+		SessionSingleton::Instance();
+	}
+}
 ?>
