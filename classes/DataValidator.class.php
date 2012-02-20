b3370b6ffb01bd849cc5632a22f29f7f891a06b8
Shawn LeMaster <shlemas@siue.edu>
2012-02-20 02:19:54 -0600

    Added initial DataValidator class.

---
+++ classes/DataValidator.class.php
@@ -0,0 +1,33 @@
+<?php
+
+class DataValidator
+{
+	private function dateTimeErrors()
+	{
+		$errs = date_get_last_errors();  
+    	return ($errs['error_count'] > 0) || ($errs['warning_count'] > 0);
+	}
+
+	// $date format:
+	//	'12/25/2011', '12-25-2011', '12.25.2011', or '12 25 2011'
+	// $time format:
+	//	'11:00 PM', '1:00 AM', '09:30 PM', etc.
+	public function validDateTime($date, $time)
+	{
+		$dateR = preg_replace('/[-\. ]/', '/', $date);
+		$dateString = $dateR . " " . $time;
+		$dateObj = date_create_from_format("m/d/Y g:i a", $dateString);
+
+		if ($dateObj && !$this->dateTimeErrors())
+			return date_timestamp_get($dateObj);
+
+		return false;
+	}
+
+	public functin validVolunteerCount($count)
+	{
+		return $count > 0; // && $count <= maxAllowedVolunteers
+	}
+}
+
+?>
