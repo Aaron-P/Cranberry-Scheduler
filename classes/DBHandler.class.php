dbf166a126d2b9c6d0cdfcf383f851cb3b6a4582
Shawn LeMaster <shlemas@siue.edu>
2012-02-26 11:34:15 -0600

    Use PDO in DBHandler.

--- classes/DBHandler.class.php
+++ classes/DBHandler.class.php
@@ -1,56 +1,48 @@
 <?php
 
-class DBSingleton
+define("dbHost", "127.0.0.1");
+define("dbPort", "3306");
+define("dbUser", "root");
+define("dbPassword", "");
+define("dbName", "cranberryscheduler");
+
+class DBHandler
 {
-	private static $dbInstance;
-	private $connection = false;
-	private $host = "localhost:3306";
-	private $userName = "root";
-	private $password = "";
-	private $dbName = "cranberryscheduler";
+	private $host;
+	private $port;
+	private $user;
+	private $password;
+	private $dbName;
+	private $dsn;
 
-	private function __construct()
-	{
-		$this->connect();
-	}
-
-	private function connect()
-	{
-		$this->connection = mysql_connect($this->host, $this->userName, $this->password);
-		if ($this->connection)
-		{
-			if (mysql_select_db($this->dbName))
-				return true;
-			$this->disconnect();
-		}
-		return false;
-	}
-
-	private function disconnect()
-	{
-		if ($this->connection)
-			return mysql_close($this->connection);
-	}
-
-	protected static function Instance()
-	{
-		if (!self::$dbInstance)
-			self::$dbInstance = new DBSingleton();
-		return self::$dbInstance;
-	}
-
-	public function query($sql)
-	{
-		$escSql = mysql_real_escape_string($sql);
-		return mysql_query($escSql);
-	}
-}
-
-class DBHandler extends DBSingleton
-{
 	public function __construct()
 	{
-		DBSingleton::Instance();
+		$this->host = dbHost;
+		$this->port = dbPort;
+		$this->user = dbUser;
+		$this->password = dbPassword;
+		$this->dbName = dbName;
+		$this->dsn = 'mysql:host='		. $this->host
+						. ';port='		. $this->port
+						. ';dbname='	. $this->dbName;
+	}
+
+	public function query($sql, $arr)
+	{
+		try
+		{
+			$connection = new PDO($this->dsn, $this->user, $this->password);
+			$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+			$sth = $connection->prepare($sql,
+				array(PDO::ATTR_CURSOR => PDO::CURSOR_SCROLL));
+			$result = $sth->execute($arr);
+			$connection = null;
+			return $sth->fetchAll();
+		}
+		catch (PDOException $e)
+		{
+			echo "Database error: " . $e->getMessage() . "<br />";
+		}
 	}
 }
 
