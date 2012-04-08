c9d9212a315639f34204dfbfe96cc9e18c7c31a0
Shawn LeMaster <shlemas@siue.edu>
2012-04-08 02:39:31 -0500

    We already format the date and time into a single string in our js code, so fixed validDateTime function in DataValidator to take this into account.

--- classes/DataValidator.class.php
+++ classes/DataValidator.class.php
@@ -11,21 +11,18 @@ class DataValidator
 		return ($errs['error_count'] > 0) || ($errs['warning_count'] > 0);
 	}
 
-	// $date format:
-	//	'12/25/2011', '12-25-2011', '12.25.2011', or '12 25 2011'
-	// $time format:
-	//	'11:00 PM', '1:00 AM', '09:30 PM', etc.
-	public function validDateTime($date, $time)
+
+	public function validDateTime($datetime)
 	{
-		$dateR = preg_replace('/[-\. ]/', '/', $date);
-		$dateString = $dateR . " " . $time;
-		$dateObj = date_create_from_format("m/d/Y g:i a", $dateString);
+		$datetimeR = preg_replace('/[-\.]/', '/', $datetime);
+		$dateObj = date_create_from_format("m/d/Y g:i a", $datetimeR);
 
 		if ($dateObj && !$this->dateTimeErrors())
 			return date_timestamp_get($dateObj);
 
 		return false;
 	}
+
 
 	public function checkRange($min, $max, $val)
 	{
