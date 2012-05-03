2816d4fd4b451ec6b0d75f3872a2dc892ca96bb9
Shawn LeMaster <shlemas@siue.edu>
2012-05-03 01:17:45 -0500

    Commented the DataValidator class.

--- classes/DataValidator.class.php
+++ classes/DataValidator.class.php
@@ -1,10 +1,26 @@
 <?php
 /**
+ * Defines the DataValidator class.
+ * @file      DataValidator.class.php
+ * @author    Aaron Papp
+ * @author    Shawn LeMaster
+ * @version   1.0
+ * @date      2011-2012
  * @copyright University of Illinois/NCSA Open Source License
  */
 
+/**
+ * Handles validating user data that comes off website forms.
+ * @class DataValidator
+ */
 class DataValidator
 {
+	/**
+     * Checks if any errors occured when calling date_create_from_format
+     * in the validDateTime function below. This is a workaround for PHP's
+     * excessive permissiveness in allowing and attempting to correct
+     * invalid dates/times.
+     */
 	private function dateTimeErrors()
 	{
 		$errs = date_get_last_errors();
@@ -12,6 +28,11 @@ class DataValidator
 	}
 
 
+	/**
+     * Validates that the string passed in represents a valid date and time
+     * to be used for scheduling a meeting.
+     * @param[in] $datetime A date and time as a string in the format e.g. "12/25/2012 1:30 PM"
+     */
 	public function validDateTime($datetime)
 	{
 		$datetimeR = preg_replace("/[-\.]/", "/", $datetime);
@@ -24,6 +45,12 @@ class DataValidator
 	}
 
 
+	/**
+     * Validates that a value falls within a certain range.
+     * @param[in] $min The minimum bound of the range
+     * @param[in] $max The maximum bound of the range
+     * @param[in] $val The value to check
+     */
 	public function checkRange($min, $max, $val)
 	{
 		return $val >= $min && $val <= $max;
